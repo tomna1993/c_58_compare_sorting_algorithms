1fd2940aa935df33722a833e0fe280de4bb1c27b
TomasNagy <tomi.nagy@outlook.com>
2023-06-05 22:35:25 +0200

    feat: add selection sort algorithm

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CC := gcc
 #CFLAGS := 
 DEST := ./build/
 
-all: clean sort1 sort2 sort3
+all: clean sort1 sort2 sort3 sort4
 
 sort1: sort1.c
 	mkdir -p build
@@ -15,6 +15,10 @@ sort2: sort2.c
 sort3: sort3.c
 	mkdir -p build
 	$(CC) sort3.c -lcs50 -o $(DEST)/sort3
+
+sort4: sort4.c
+	mkdir -p build
+	$(CC) sort4.c -lcs50 -o $(DEST)/sort4
 
 clean:
 	rm -rf $(DEST)
---
+++ sort4.c
@@ -0,0 +1,107 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int find_number(string text);
+void selection_sort(int array[], int array_length);
+void print_array(int array[], int array_length);
+
+int main(int argc, string argv[])
+{
+    // Show user how to start the program if it was started incorrectly
+    if (argc != 2)
+    {
+        printf ("Usage: ./[program_name] [text_file.txt]\n");
+        printf ("or: time ./[program_name] [text_file.txt]\n");
+        printf ("Example: time ./sort1 reversed10000.txt\n");
+        return 1;
+    }
+
+    string text_file = argv[1];
+
+    int list_length = find_number(text_file);
+
+    int unsorted_numbers[list_length];
+
+    // Read file into array
+    FILE *file_handle = fopen (text_file, "r");
+
+    if (file_handle == NULL)
+    {
+        printf ("Cannot open file!\n");
+        return 1;
+    }
+
+    for (int i = 0; i < list_length; i++)
+    {
+        fscanf(file_handle, "%i", &unsorted_numbers[i]);
+    }
+
+    selection_sort(unsorted_numbers, list_length);
+   
+    // print_array(unsorted_numbers, list_length);
+
+    return 0;
+}
+
+int find_number(string text)
+{
+    char number[10];
+    int j = 0;
+
+    for (int i = 0, length = strlen(text); i < length; i++)
+    {
+        if (isalpha(text[i]))
+        {
+            continue;
+        }
+
+        if (isdigit(text[i]))
+        {
+            number[j++] = text[i];   
+        }
+    }
+
+    return atoi(number);
+}
+
+void selection_sort(int array[], int array_length)
+{
+    for (int i = 0; i < array_length-1; i++)
+    {
+        int smallest_index = i;
+
+        for (int j = i + 1; j < array_length; j++)
+        {
+            if (array[smallest_index] > array[j])
+            {
+                smallest_index = j;   
+            }
+        }
+
+        // Swap the lowest value with the i-th place
+        int temp = array[i];
+        array[i] = array[smallest_index];
+        array[smallest_index] = temp; 
+    }
+
+    return;
+}
+
+void print_array(int array[], int array_length)
+{
+    for (int i = 0; i < array_length; i++)
+    {
+        if (i % 20 == 0)
+        {
+            printf ("\n");
+        }
+        printf ("%i ", array[i]);
+    }
+    printf ("\n");
+
+    return;
+}
+
