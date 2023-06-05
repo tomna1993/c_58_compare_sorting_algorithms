743dcaf3f5154d71e1985fbcac7f35301a57cde2
TomasNagy <tomi.nagy@outlook.com>
2023-06-05 12:49:06 +0200

    feat: check if  CLA input is correct

--- sort2.c
+++ sort2.c
@@ -8,6 +8,16 @@ void bubble_sort(int unsorted_array[], int array_length);
 
 int main(int argc, string argv[])
 {
+    // Show user how to start the program if it was started incorrectly
+    if (argc != 2)
+    {
+        printf ("Usage: ./[program_name] [text_file.txt]\n");
+        printf ("or: time ./[program_name] [text_file.txt]\n");
+        printf ("Example: time ./sort1 reversed10000.txt\n");
+        return 1;
+    }
+
+
     int unsorted_array[ARRAY_LENGTH] = {0, 4, 3, 10, 6, 2, 8, 7, 5, 9, 1};
 
     printf ("Unsorted: ");
